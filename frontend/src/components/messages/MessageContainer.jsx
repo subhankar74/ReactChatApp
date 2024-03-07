2d0f4995ae14a63ece802cf023e0fb689811a0bc
subhankar mondal <subhoharry422potter@gmail.com>
2024-03-07 12:20:49 +0530

    3rd part is completed

--- frontend/src/components/messages/MessageContainer.jsx
+++ frontend/src/components/messages/MessageContainer.jsx
@@ -1,35 +1,49 @@
+import { useEffect } from "react";
 import Messages from "./Messages.jsx";
 import MessageInput from "./MessageInput.jsx";
 import { TiMessages } from "react-icons/ti";
+import useConversation from "../../zustand/useConversation.js";
 const MessageContainer = () => {
-	const noChatSelected=false;
-    	return (
-    		<div className='md:min-w-[450px] flex flex-col'>
-    			{noChatSelected ? (<NoChatSelected/>):(
-					<>
-    				{/* Header */}
-    				<div className='bg-slate-500 px-4 py-2 mb-2'>
-    					<span className='label-text'>To:</span> <span className='text-gray-900 font-bold'>Subhankar Mondal</span>
-    				</div>
-    
-    				<Messages />
-    				<MessageInput />
-    			</>
-				)}
-    		</div>
-    	);
-    };
+  // const noChatSelected = true;
+  const { selectedConversation, setSelectedConversation } = useConversation();
+  useEffect(() => {
+    // cleanup function (unmounts)
+    return () => setSelectedConversation(null);
+  }, [setSelectedConversation]);
+  return (
+    <div className="md:min-w-[450px] flex flex-col">
+      {!selectedConversation ? (
+        <NoChatSelected />
+      ) : (
+        <>
+          {/* Header */}
+          <div className="bg-slate-500 px-4 py-2 mb-2">
+            <span className="label-text">To:</span>{" "}
+            <span className="text-gray-900 font-bold">
+              {selectedConversation.fullName}
+            </span>
+          </div>
+
+          <Messages />
+          <MessageInput />
+        </>
+      )}
+    </div>
+  );
+};
 export default MessageContainer;
 
 const NoChatSelected = () => {
-	// const { authUser } = useAuthContext();
-	return (
-		<div className='flex items-center justify-center w-full h-full'>
-			<div className='px-4 text-center sm:text-lg md:text-xl text-gray-200 font-semibold flex flex-col items-center gap-2'>
-				<p>Welcome 👋 Subhankar Mondal ❄</p>
-				<p>Select a chat to start messaging</p>
-				<TiMessages className='text-3xl md:text-6xl text-center' />
-			</div>
-		</div>
-	);
+  // const { authUser } = useAuthContext();
+  return (
+    <div className="flex items-center justify-center w-full h-full">
+      <div className="px-4 text-center sm:text-lg md:text-xl text-gray-200 font-semibold flex flex-col items-center gap-2">
+        <p>Welcome 👋 Subhankar Mondal ❄</p>
+        <p>Select a chat to start messaging</p>
+        <TiMessages className="text-3xl md:text-6xl text-center" />
+
+        <p>End-to-End encrypted</p>
+      </div>
+    </div>
+  );
 };
